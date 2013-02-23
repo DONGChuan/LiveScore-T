d847982edcfaf411de39c3e2e48c8c5ad4b6c666
DONGChuan <dongchuan55@gmail.com>
2013-02-23 08:20:19 -0800

    Add 4 button on mainpage

--- LiveScore/gen/com/tse/livescore/activity/R.java
+++ LiveScore/gen/com/tse/livescore/activity/R.java
@@ -14,16 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int basketball_listview=0x7f070005;
-        public static final int football_listview=0x7f070003;
+        public static final int add=0x7f070005;
+        public static final int basketball_listview=0x7f070009;
+        public static final int football_listview=0x7f070007;
         public static final int imageView1=0x7f070002;
+        public static final int list=0x7f070003;
         public static final int live=0x7f070000;
-        public static final int menu_settings=0x7f070009;
-        public static final int rugby_listview=0x7f070004;
-        public static final int tab1=0x7f070006;
-        public static final int tab2=0x7f070007;
-        public static final int tab3=0x7f070008;
+        public static final int menu_settings=0x7f07000d;
+        public static final int rugby_listview=0x7f070008;
+        public static final int search=0x7f070004;
+        public static final int tab1=0x7f07000a;
+        public static final int tab2=0x7f07000b;
+        public static final int tab3=0x7f07000c;
         public static final int textView1=0x7f070001;
+        public static final int update=0x7f070006;
     }
     public static final class layout {
         public static final int activity_detail__live__page=0x7f030000;
@@ -39,17 +43,21 @@ public final class R {
         public static final int activity_my_tab_listener=0x7f060004;
     }
     public static final class string {
-        public static final int app_name=0x7f040003;
-        public static final int basketball=0x7f040001;
-        public static final int football=0x7f040000;
-        public static final int hell_world=0x7f040005;
-        public static final int hello_world=0x7f040004;
-        public static final int menu_settings=0x7f040006;
-        public static final int rugby=0x7f040002;
-        public static final int title_activity_detail__live__page=0x7f040009;
-        public static final int title_activity_list_view_loader=0x7f040008;
-        public static final int title_activity_main__page=0x7f040007;
-        public static final int title_activity_my_tab_listener=0x7f04000a;
+        public static final int add=0x7f040002;
+        public static final int app_name=0x7f040007;
+        public static final int basketball=0x7f040005;
+        public static final int football=0x7f040004;
+        public static final int hell_world=0x7f040009;
+        public static final int hello_world=0x7f040008;
+        public static final int list=0x7f040000;
+        public static final int menu_settings=0x7f04000a;
+        public static final int rugby=0x7f040006;
+        public static final int search=0x7f040001;
+        public static final int title_activity_detail__live__page=0x7f04000d;
+        public static final int title_activity_list_view_loader=0x7f04000c;
+        public static final int title_activity_main__page=0x7f04000b;
+        public static final int title_activity_my_tab_listener=0x7f04000e;
+        public static final int update=0x7f040003;
     }
     public static final class style {
         /** 
