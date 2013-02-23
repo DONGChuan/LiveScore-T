99987d091bba1f1df3d7e6f2499dd8bba54df965
DONGChuan <dongchuan55@gmail.com>
2013-02-23 13:43:07 -0800

    icon

--- LiveScore/gen/com/tse/livescore/activity/R.java
+++ LiveScore/gen/com/tse/livescore/activity/R.java
@@ -12,16 +12,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int launch_page=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f070006;
-        public static final int basketball_listview=0x7f070005;
-        public static final int football_listview=0x7f070003;
-        public static final int imageView1=0x7f070002;
+        public static final int LinearLayout01=0x7f070005;
+        public static final int basketball_listview=0x7f070004;
+        public static final int football_listview=0x7f070002;
+        public static final int imageView1=0x7f070001;
         public static final int live=0x7f070000;
-        public static final int menu_settings=0x7f070007;
-        public static final int rugby_listview=0x7f070004;
-        public static final int textView1=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int rugby_listview=0x7f070003;
     }
     public static final class layout {
         public static final int activity_detail__live__page=0x7f030000;
