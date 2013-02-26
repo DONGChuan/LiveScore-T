6b45473bc926ff37f15dafccc633bfb2911f3a18
DONGChuan <dongchuan55@gmail.com>
2013-02-26 02:56:07 -0800

    background

--- LiveScore/gen/com/tse/livescore/activity/R.java
+++ LiveScore/gen/com/tse/livescore/activity/R.java
@@ -21,16 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int basketball=0x7f020000;
-        public static final int football=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int launch_page=0x7f020003;
-        public static final int logo=0x7f020004;
-        public static final int moins=0x7f020005;
-        public static final int plus=0x7f020006;
-        public static final int rugby=0x7f020007;
-        public static final int search=0x7f020008;
-        public static final int vs=0x7f020009;
+        public static final int background=0x7f020000;
+        public static final int basketball=0x7f020001;
+        public static final int football=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int launch_page=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int moins=0x7f020006;
+        public static final int plus=0x7f020007;
+        public static final int rugby=0x7f020008;
+        public static final int search=0x7f020009;
+        public static final int vs=0x7f02000a;
     }
     public static final class id {
         public static final int LinearLayout01=0x7f08002e;
